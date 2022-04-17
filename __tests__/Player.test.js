05a242737e744290d6e948a165e7f285ade9c97e
escowin <edwin@escowinart.com>
2022-04-17 15:15:04 -0500

    pre-redux

---
+++ __tests__/Player.test.js
@@ -0,0 +1,34 @@
+const Player = require('../lib/Player');
+const Potion = require('../lib/Potion');
+
+jest.mock('../lib/Potion');
+console.log(new Potion());
+
+test('get players stats as an object', () => {
+    const player = new Player('dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
+});
+
+// test('creates a player object', () => {
+//     const player = new Player('Dave');
+
+//     expect(player.name).toBe('Dave');
+//     expect(player.health).toEqual(expect.any(Number));
+//     expect(player.strength).toEqual(expect.any(Number));
+//     expect(player.agility).toEqual(expect.any(Number));
+//     expect(player.inventory).toEqual(
+//         expect.arrayContaining([expect.any(Object)])
+//     );
+// });
