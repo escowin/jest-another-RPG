85c4d90652a21e14406add24e623d6ea4b63b112
escowin <edwin@escowinart.com>
2022-04-17 17:32:23 -0500

    10.2.8 | code snapshot

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,35 +1,34 @@
-const { expect, test } = require('@jest/globals');
-const exp = require('constants');
-const Player = require('../lib/Player');
-const Potion = require('../lib/Potion');
-jest.mock('../lib/Potion');
-console.log(new Potion());
+const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
 
-// GitHub issue | want players to have a name and three number properties: health, strength, and agility. Write test that checks for the existence of those four things.
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
-    const player = new Player('dave');
+  const player = new Player('Dave');
 
-    expect(player.name).toBe('dave');
-    expect(player.health).toEqual(expect.any(Number));
-    expect(player.strength).toEqual(expect.any(Number));
-    expect(player.agility).toEqual(expect.any(Number));
-    expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+  expect(player.name).toBe('Dave');
+  expect(player.health).toEqual(expect.any(Number));
+  expect(player.strength).toEqual(expect.any(Number));
+  expect(player.agility).toEqual(expect.any(Number));
+
+  expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
 
-test("get player stats as an object", () => {
-    const player = new Player('dave');
+test("gets player's stats as an object", () => {
+  const player = new Player('Dave');
 
-    expect(player.getStats()).toHaveProperty('potions');
-    expect(player.getStats()).toHaveProperty('health');
-    expect(player.getStats()).toHaveProperty('strength');
-    expect(player.getStats()).toHaveProperty('agility');
+  expect(player.getStats()).toHaveProperty('potions');
+  expect(player.getStats()).toHaveProperty('health');
+  expect(player.getStats()).toHaveProperty('strength');
+  expect(player.getStats()).toHaveProperty('agility');
 });
 
-test('get inventory from player or returns false', () => {
-    const player = new Player('dave');
+test('gets inventory from player or returns false', () => {
+  const player = new Player('Dave');
 
-    expect(player.getInventory()).toEqual(expect.any(Array));
-    player.inventory = [];
-    expect(player.getInventory()).toEqual(false);
+  expect(player.getInventory()).toEqual(expect.any(Array));
+
+  player.inventory = [];
+
+  expect(player.getInventory()).toEqual(false);
 });
