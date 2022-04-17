4a1355341ade1b58ee3f453a98034f2334ba7da0
escowin <edwin@escowinart.com>
2022-04-17 17:19:31 -0500

    10.2.7 | tdd getStats getInventory tests pass

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # jest-another-RPG
 * 10 | Object-Oriented Programming
+
+
+## mocking data
+* see 10.2.5:
+const fs = require('fs');
+jest.mock('fs');
+fs.readFileSync.mockReturnValue('fake content');
--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,4 +1,5 @@
-const { expect } = require('@jest/globals');
+const { expect, test } = require('@jest/globals');
+const exp = require('constants');
 const Player = require('../lib/Player');
 const Potion = require('../lib/Potion');
 jest.mock('../lib/Potion');
@@ -14,4 +15,21 @@ test('creates a player object', () => {
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
+});
+
+test("get player stats as an object", () => {
+    const player = new Player('dave');
+
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+});
+
+test('get inventory from player or returns false', () => {
+    const player = new Player('dave');
+
+    expect(player.getInventory()).toEqual(expect.any(Array));
+    player.inventory = [];
+    expect(player.getInventory()).toEqual(false);
 });
