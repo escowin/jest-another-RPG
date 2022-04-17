56f918b71fecb60b49c38e7caf433190639cfefb
escowin <edwin@escowinart.com>
2022-04-17 16:38:26 -0500

    10.2.4 | TDD initial test fail

---
+++ __tests__/Player.test.js
@@ -0,0 +1,13 @@
+const { test, expect } = require('@jest/globals');
+const Player = require('../lib/Player');
+
+// GitHub issue | want players to have a name and three number properties: health, strength, and agility. Write test that checks for the existence of those four things.
+
+test('creates a player object', () => {
+    const Player = new Player('dave');
+
+    expect(player.name).toBe('dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+});
