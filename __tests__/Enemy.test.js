d361e60f6e23688fb2e1faa005ec9e08124d59a8
escowin <edwin@escowinart.com>
2022-10-04 17:26:55 -0500

    10.3.6 | tdd Enemy methods

--- __tests__/Enemy.test.js
+++ __tests__/Enemy.test.js
@@ -14,3 +14,47 @@ test('create an enemy object', () => {
     expect(enemy.agility).toEqual(expect.any(Number));
     expect(enemy.potion).toEqual(expect.any(Object));
 });
+
+test('get enemy health value', () => {
+    const enemy = new Enemy('Pet Shop', 'horus');
+
+    expect(enemy.getHealth()).toEqual(expect.stringContaining(enemy.health.toString()));
+});
+
+test('checks whether or not enemy is alive', () => {
+    const enemy = new Enemy('Oingo', 'khnum');
+
+    expect(enemy.isAlive()).toBeTruthy();
+
+    enemy.health = 0;
+
+    expect(enemy.isAlive()).toBeFalsy();
+});
+
+test('get enemy attack value', () => {
+    const enemy = new Enemy('Vanilla Ice', 'Cream');
+    enemy.strength = 10;
+
+    expect(enemy.getAttackValue()).toBeGreaterThanOrEqual(5);
+    expect(enemy.getAttackValue()).toBeLessThanOrEqual(15);
+});
+
+test('subtract from enemy hp', () => {
+    const enemy = new Enemy('Kira', 'Killer Queen');
+    const oldHealth = enemy.health;
+
+    enemy.reduceHealth(5);
+
+    expect(enemy.health).toBe(oldHealth - 5);
+
+    enemy.reduceHealth(99999);
+
+    expect(enemy.health).toBe(0);
+});
+
+test('gets a description of the enemy', () => {
+    const enemy = new Enemy('Iggy', 'the fool');
+
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('Iggy'));
+    expect(enemy.getDescription()).toEqual(expect.stringContaining('the fool'));
+});
